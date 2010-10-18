2fd712c8c0d86fe5dfef4295992d54a1aaf1e804
Alexander Gräfenstein <giniedp@online.de>
2010-10-18 18:35:15 +0200

    -

--- lib/result.rb
+++ lib/result.rb
@@ -6,7 +6,7 @@ module Railsgrid
       if table.model_class < ActiveRecord::Base
         self.values      = table.model_class.find(:all, table.query)
         count_query = table.query.reject{ |k, v| [:limit, :offset, :order].include? k }
-        self.total       = table.model_class.count(:all, count_query).length
+        #self.total       = table.model_class.count(:all, count_query).length
       elsif table.model_class < ActiveResource::Base
         self.values = table.model_class.find(:all, :params => table.query)
         self.total  = self.values.delete_at(values.length - 1).total
